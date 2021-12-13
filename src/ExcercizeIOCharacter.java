c148c5aad3d71701904ebaf39d62401c6f8c6998
tgeronimo <thi.matheus@gmail.com>
2021-12-13 06:22:09 -0300

    Initial commit

---
+++ src/ExcercizeIOCharacter.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class ExcercizeIOCharacter {
+    public static void main(String[] args) throws IOException {
+        readInputPrintOutput();
+    }
+
+    public static void readInputPrintOutput() throws IOException {
+
+        System.out.println("List 3 movies: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do {
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        } while (line.isBlank());
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
