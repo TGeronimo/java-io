1493e84d35715733b40d451bbf3d7026ac6393da
TGeronimo <thi.matheus@gmail.com>
2021-12-14 14:48:07 -0300

    deserialization exercize completed

--- src/objects/Cat.java
+++ src/objects/Cat.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 public class Cat implements Serializable {
 
+    private static final long serialVersionUID = 2L;
     String nameCat;
     int ageCat;
     String colorCat;
+    transient boolean roam;
 
     public Cat(String nameCat, int ageCat, String colorCat) {
         this.nameCat = nameCat;
